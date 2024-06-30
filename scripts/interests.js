818c186251576e456f459f53d909fd85d9adc2cb
Keeity Braga Collodel <keeity@hotmail.com>
2024-06-30 00:13:07 -0300

    versao de lançamento 1.0 conferida

--- scripts/interests.js
+++ scripts/interests.js
@@ -1,20 +1,22 @@
 const addBtn = document.getElementById("addBtn")
 const clearBtn = document.getElementById("clearBtn")
 
-
+//Adicionar interesses e hobbies
 function addInterest () {
     
     const interestI = document.getElementById("interestI")
     const interestValue = interestI.value.trim();
     if(interestValue)              {
+        
         const listLS = JSON.parse(localStorage.getItem('meus-interesses')) || [];
     
   
         listLS.push(interestValue);
         localStorage.setItem('meus-interesses', JSON.stringify(listLS));
     
-        addInterest.value = "";
+       interestI.value = "";
         addInterestsList();
+
     } else{
     alert("Escreva um interesse ou hobby antes de enviar!");
     }
@@ -29,9 +31,9 @@ function addInterestsList() {
         const item = document.createElement('li');
        item.textContent = interest; 
         iList.appendChild(item);
+            
     });
 }
-
 
     function clearInterestsList() {
         localStorage.removeItem('meus-interesses');
